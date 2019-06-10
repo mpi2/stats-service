b4e8072b4ff13bb9d4330689c346961a7987f85e
jwarren <warren.jonathan@gmail.com>
2019-06-10 10:20:32 +0100

    sex_fvko_estimate changed to String as percentage also

--- src/main/java/uk/ac/ebi/phenotype/stats/model/NormalResult.java
+++ src/main/java/uk/ac/ebi/phenotype/stats/model/NormalResult.java
@@ -257,11 +257,11 @@ public class NormalResult {
 		this.interactionPVal = interactionPVal;
 	}
 
-	public Double getSexFvkoEstimate() {
+	public String getSexFvkoEstimate() {
 		return sexFvkoEstimate;
 	}
 
-	public void setSexFvkoEstimate(Double sexFvkoEstimate) {
+	public void setSexFvkoEstimate(String sexFvkoEstimate) {
 		this.sexFvkoEstimate = sexFvkoEstimate;
 	}
 
@@ -348,7 +348,7 @@ public class NormalResult {
 	@JsonProperty("interaction_p_val")
 	private Double interactionPVal;// : 0.623132324183186,
 	@JsonProperty("sex_fvko_estimate")
-	private Double sexFvkoEstimate;// : {},
+	private String sexFvkoEstimate;// : {},
 	@JsonProperty("sex_fvko_standard_error")
 	private Double sexFvkoStandardError;// : {},
 	@JsonProperty("sex_fvko_p_val")
